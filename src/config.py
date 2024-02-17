d25287825e85120b7f1b5a64d713684b049b2735
Sameer Ahmad <samimughal7786@gmail.com>
2024-02-17 17:12:23 +0500

    feature-pipeline has been added

---
+++ src/config.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.append("..\src")
+import os
+from dotenv import load_dotenv
+from paths import PARENT_DIR
+load_dotenv(PARENT_DIR / '.env')
+
+HOPSWORKS_PROJECT_NAME = 'TaxiDemandPredictorNYC'
+try:
+    # HOPSWORKS_PROJECT_NAME = os.environ['HOPSWORKS_PROJECT_NAME']
+    HOPSWORKS_API_KEY = os.environ['HOPSWORKS_API_KEY']
+except:
+    raise Exception('Create an .env file on the project root with the HOPSWORKS_API_KEY')
+FEATURE_GROUP_NAME = 'time_series_hourly_feature_group'
+FEATURE_GROUP_VERSION = 3
