264dc63c539e3bd2e3b0085f4104d12109f1015f
Sameer Ahmad <samimughal7786@gmail.com>
2024-07-13 14:43:12 +0500

    updated

--- src/data_split.py
+++ src/data_split.py
@@ -10,7 +10,20 @@ def train_test_split(
     target_column_name: str,
 ) -> Tuple[pd.DataFrame, pd.Series, pd.DataFrame, pd.Series]:
     """
+    Splits the dataframe into training and testing datasets based on a cutoff date.
     """
+    # Ensure pickup_hour column is of datetime type
+    df['pickup_hour'] = pd.to_datetime(df['pickup_hour'])
+
+    # Check if cutoff_date is timezone-aware and handle accordingly
+    if cutoff_date.tzinfo is not None:
+        # Convert pickup_hour to timezone-aware
+        df['pickup_hour'] = df['pickup_hour'].dt.tz_localize(
+            cutoff_date.tzinfo)
+    else:
+        # Remove timezone info from cutoff_date
+        cutoff_date = cutoff_date.replace(tzinfo=None)
+
     train_data = df[df.pickup_hour < cutoff_date].reset_index(drop=True)
     test_data = df[df.pickup_hour >= cutoff_date].reset_index(drop=True)
 
